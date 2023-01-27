20934d776e8c905fd82ab3bc3fad9081791b97cf
Kumar Sharma <nkumarsharma@deloitte.com>
2023-01-27 16:53:14 +0530

    updating main.tf

--- main.tf
+++ main.tf
@@ -3,3 +3,7 @@ resource "google_storage_bucket" "bucket" {
   location = "europe-west2"
 }
 
+resource "google_storage_bucket" "gcs_bucket" {
+  name     = "test-bucket-random-00112323"
+  location = "europe-west2"
+}
