a5c80fe2b72f047d6954479ca1171fb5752a77e0
Nitesh Sharma <75922520+niteshsharma99@users.noreply.github.com>
2023-02-08 12:02:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,10 @@
+#bucket1
 resource "google_storage_bucket" "bucket" {
   name     = "test-bucket-random-001122-22"
   location = "europe-west2"
 }
 
+#bucket2
 resource "google_storage_bucket" "gcs_bucket" {
   name     = "test-bucket-random-001123231112"
   location = "europe-west2"
